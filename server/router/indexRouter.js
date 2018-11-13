b3d04e03d28a0b7b7befe524b1d2ac29f94d83dc
kim <kimberlymae68@gmail.com>
2018-11-13 14:35:18 +0800

    added cookies

--- server/router/indexRouter.js
+++ server/router/indexRouter.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const cookieParser = require('cookie-parser');
+
+router.use(cookieParser());
 
 router.get('/', (req, res) => {
-    let viewModel = req.viewModel;
-    res.render('index.pug', viewModel);
+    if(req.cookies['merchantId']){
+        let viewModel = req.viewModel;
+        res.render('index.pug', viewModel);
+    }else{
+        res.json({'cookies':'No cookies'});
+    }
+    
 });
+
+router.get('/login', (req, res) => {
+    res.cookie('merchantId', 1000000000, { maxAge: 24 * 60 * 60 * 1000, httpOnly: true });
+    res.redirect('/')
+})
+
+router.get('/logout', (req, res) => {
+    res.clearCookie('merchantId');
+    res.redirect('/');
+})
+
 
 module.exports = router;
