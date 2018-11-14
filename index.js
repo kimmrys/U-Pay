e8cf889d00e57e756239eaad56381c0e3654a7e5
kimmrys <kimberlymae68@gmail.com>
2018-11-14 18:18:29 +0800

    latest

--- index.js
+++ index.js
@@ -1,28 +1,44 @@
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
 const port = 3000;
 
 const indexRouter = require('./server/router/indexRouter');
 const merchantRouter = require('./server/router/merchantRouter');
 const transactionRouter = require('./server/router/transactionRouter');
+const historyRouter = require('./server/router/historyRouter');
 const refundRouter = require('./server/router/refundRouter');
-const summaryRouter = require('./server/router/summaryRouter');
-const settleRouter = require('./server/router/settleRouter');
 
+const showPayment = require('./server/api/showPayment');
 
 const app = express();
+app.use(cookieParser());
 
 app.use(express.static('public'));
 
 app.set('views', path.join(__dirname, 'server/views'));
 app.set('view engine', 'pug');
 
+app.use('/',(req, res, next) => {
+   
+    let merchantId = req.cookies['merchantId'];
+    if(merchantId){
+        next();
+    }else if(req.path === '/login'){
+        next();
+    }
+    else{
+        res.json({'cookies':'No cookies'});
+    }
+})
+
 app.use('/', indexRouter);
 app.use('/merchant', merchantRouter);
 app.use('/transaction', transactionRouter);
+app.use('/history', historyRouter);
 app.use('/refund', refundRouter);
-app.use('/summary', summaryRouter);
-app.use('/settlement', settleRouter);
+
+app.use('/api', showPayment);
 
 app.listen(port, (err) => {
     if(err) {
@@ -30,3 +46,4 @@ app.listen(port, (err) => {
     }
     console.log(`Listening to ${port}...`);
 });
+
