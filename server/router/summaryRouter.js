b1f050f9768143a3f7d70192c811e320a15ee24d
pat <patpalmero@gmail.com>
2018-11-14 17:47:10 +0800

    my commit

---
+++ server/router/summaryRouter.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/', (req, res) => {
+    let viewModel = req.viewModel;
+    res.render('summary.pug', viewModel);
+});
+
+module.exports = router;
